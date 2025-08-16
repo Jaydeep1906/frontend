ba6ad7a7e8345bfda24a544198d3f6ba27228317
Jaydeep1906 <jaydeepskadam19@gmail.com>
2025-08-16 15:35:57 +0530

    Add files via upload

---
+++ JavascriptClass/Arithmatic.js
@@ -0,0 +1,7 @@
+// var a= 5;
+
+// var b= 7;
+
+// var sum = a+b;
+
+// console.log(sum); 
---
+++ JavascriptClass/External.js
@@ -0,0 +1,3 @@
+document.write( <b> "This is external file"</b>);
+document.write("this is print");
+document.write("This is javascript");
